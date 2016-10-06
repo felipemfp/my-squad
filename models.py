fc0fc670427e34f33c5f793bb70d61372e8d07c2
Felipe Pontes <felipemfpontes@gmail.com>
2016-10-06 07:03:49 -0300

    Add poor implementation of players evolution (#4)

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from enum import Enum
 from itertools import permutations
 from tabulate import tabulate
@@ -31,15 +32,46 @@ class Position(Enum):
 
 class Player(object):
 
-    def __init__(self, name, position, age, rating=60, potencial=75):
+    def __init__(self, name, position, age, rating=60, potencial=75,
+                 growth_factor=15.0, growth_ages=3):
+        self.__id = str(uuid4())
         self.name = name
         self.position = position
         self.age = age
         self.potencial = potencial
         self.rating = rating
+        self.experience = 0.0
+        self.growth_factor = growth_factor
+        self.growth_max_age = self.age + growth_ages
+
+    @property
+    def id(self):
+        return self.__id
 
     def __repr__(self):
         return '<Player {}, {}>'.format(self.name, self.position.name)
+
+    def increase_age(self):
+        self.age += 1
+        self.growth_factor *= 1.1
+
+    def increase_experience(self, won=False, played=False, goal=False):
+        factor = 0.25 if self.age <= self.growth_max_age else 0.5
+        if goal:
+            factor = 1.0 if self.age <= self.growth_max_age else 0.20
+        elif played:
+            factor = 0.5 if self.age <= self.growth_max_age else 0.25
+        if won and self.rating < self.potencial and self.age <= self.growth_max_age:
+            factor *= 2
+        self.experience += factor
+
+    def handle_growth(self):
+        if self.experience > self.growth_factor:
+            if self.age <= self.growth_max_age:
+                self.rating += int(self.experience / self.growth_factor)
+            else:
+                self.rating -= int(self.experience / self.growth_factor)
+            self.experience = 0
 
 
 class Squad(object):
@@ -60,7 +92,9 @@ class Squad(object):
                 player.potencial
             ]
             for player in self.players]
-        return tabulate(data, headers=('POS', 'NAME', 'AGE', 'RAT', 'POT'))
+        return tabulate(data,
+                        headers=('POS', 'NAME', 'AGE', 'RAT', 'POT'),
+                        tablefmt='orgtbl')
 
 
 class Team(object):
@@ -85,9 +119,7 @@ class TeamInLeague(object):
         self.lost = lost
         self.goals_for = goals_for
         self.goals_against = goals_against
-
-    def __repr__(self):
-        return '<TeamInLeague {}>'.format(self.team.name)
+        self.goals = {}
 
     @property
     def points(self):
@@ -96,6 +128,15 @@ class TeamInLeague(object):
     @property
     def goals_difference(self):
         return self.goals_for - self.goals_against
+
+    def __repr__(self):
+        return '<TeamInLeague {}>'.format(self.team.name)
+
+    def add_goal(self, player):
+        if player.id in self.goals.keys():
+            self.goals[player.id] += 1
+        else:
+            self.goals[player.id] = 1
 
 
 class League(object):
@@ -116,4 +157,7 @@ class League(object):
                 team.goals_difference, team.points
             )
             for i, team in enumerate(self.teams)]
-        return tabulate(data, headers=('POS', 'TEAM', 'P', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'PTS'))
+        return tabulate(data,
+                        headers=('POS', 'TEAM', 'P', 'W', 'D',
+                                 'L', 'GF', 'GA', 'GD', 'PTS'),
+                        tablefmt='orgtbl')
